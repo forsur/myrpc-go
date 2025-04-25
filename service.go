b86c4a35afd26b08fdecc4d68574f782e501794f
livelyxu <livelyxu@tencent.com>
2025-04-25 18:30:08 +0800

    3

--- service.go
+++ service.go
@@ -1,6 +1,115 @@
 package myrpc
-import (
 
+import (
+	"go/ast"
+	"log"
+	"reflect"
+	"sync/atomic"
 )
+
+
+type methodType struct {
+	method reflect.Method
+	ArgType reflect.Type
+	ReplyType reflect.Type
+	numCalls uint64 // 统计方法调用次数
+}
+
+func (m *methodType) NumCalls() uint64 {
+	return atomic.LoadUint64(&m.numCalls)
+}
+
+
+func (m *methodType) newArgv() reflect.Value { // reflect.New() 分配内存，返回一个指向该内存的指针
+	var argv reflect.Value
+	if m.ArgType.Kind() == reflect.Ptr {
+		argv = reflect.New(m.ArgType.Elem()) // 拿到指针
+	} else {
+		argv = reflect.New(m.ArgType).Elem() // 拿到值
+	}
+	return argv
+}
+
+func (m *methodType) newReplyv() reflect.Value {
+	// 注意这里的 reply 一定是一个 ptr
+	replyv := reflect.New(m.ReplyType.Elem())
+	switch m.ReplyType.Elem().Kind() {
+	case reflect.Map:
+		replyv.Elem().Set(reflect.MakeMap(m.ReplyType.Elem()))
+	case reflect.Slice:
+		replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
+	}
+	return replyv
+}
+
+
+
+type service struct {
+	name string
+	typ reflect.Type
+	rcvr reflect.Value
+	method map[string]*methodType
+}
+
+// 入参为任意要映射为结构体的实例
+func newService(rcvr interface{}) *service {
+	s := new(service) // 分配内存，返回指针；内存的值为 service 结构体的零值
+	s.rcvr = reflect.ValueOf(rcvr)
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+	s.typ = reflect.TypeOf(rcvr)
+	if !ast.IsExported(s.name) {
+		log.Fatalf("rpc server: %s is not a valid service name", s.name)
+	}
+	s.registerMethods()
+	return s
+}
+
+func (s *service) registerMethods() {
+	s.method = make(map[string]*methodType)
+	for i := 0; i < s.typ.NumMethod(); i++ {
+		// 获取结构体绑定的方法
+		method := s.typ.Method(i)
+		mType := method.Type
+
+		// 检查 rpc 接口规范
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+		
+		// 两个导出或内置类型的入参（反射时为 3 个，第 0 个是自身，类似于 python 的 self，java 中的 this）
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+		s.method[method.Name] = &methodType{
+			method: method,
+			ArgType: argType,
+			ReplyType: replyType,
+		}
+		log.Printf("rpc server: register %s.%s\n", s.name, method.Name)
+	}
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
+}
+
+
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	
+	errRet := returnValues[0].Interface()
+
+	if errRet != nil {
+		return errRet.(error)
+	}
+	return nil
+}
+
 
 
